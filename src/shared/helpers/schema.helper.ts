36294bdd96a1d1143ce9e1b629fba2a08a264890
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-07 22:35:39 +0100

    feat: healper to create a embedded schema

--- src/shared/helpers/schema.helper.ts
+++ src/shared/helpers/schema.helper.ts
@@ -2,4 +2,4 @@ import { Schema } from 'mongoose'
 
 export const createDefaultSchema = (obj: unknown): Schema => new Schema(Object.assign({}, obj), { timestamps: true, versionKey: false })
 
-export const createWithoutTimestampSchema = (obj: unknown): Schema => new Schema(Object.assign({}, obj), { versionKey: false })
+export const createEmbeddedSchema = (obj: unknown): Schema => new Schema(Object.assign({}, obj), { _id: false, versionKey: false })
