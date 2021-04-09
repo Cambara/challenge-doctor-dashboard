63978392eef836cb73003e75a301fe3d1cc241dd
Tiago Cambará <tiago.cambara@gmail.com>
2021-04-09 11:07:22 +0100

    feat: change the business role to validate a body

--- src/modules/lab/lab-add/index.ts
+++ src/modules/lab/lab-add/index.ts
@@ -1,11 +1,10 @@
-import { YupProvider } from '../../../providers/validator/yup.provider'
+import { validatorService } from '../../../shared/services'
 import { LabRepository } from '../lab.repository'
 import { LabAddController } from './lab-add.controller'
 import { LabAddService } from './lab-add.service'
 
 const labRepository = LabRepository.getInstance()
-const validateProvider = YupProvider.getInstance()
 const labAddService = new LabAddService(labRepository)
-const labAddController = new LabAddController(labAddService, validateProvider)
+const labAddController = new LabAddController(labAddService, validatorService)
 
 export { labAddController, labAddService }
--- src/modules/lab/lab-add/lab-add.controller.ts
+++ src/modules/lab/lab-add/lab-add.controller.ts
@@ -2,24 +2,23 @@ import { IController } from '../../../shared/controllers/controller.protocol'
 import { Request, Response } from 'express'
 import { badRequest, internalErrorRequest, successRequest } from '../../../shared/helpers/http-response.helper'
 import { LabAddService } from './lab-add.service'
-import { IValidatorProvider } from '../../../providers/validator/validator.protocol'
 import { ILabAddDto } from './lab-add.dto'
-import { ObjectSchema } from 'yup'
 import { validator } from './lab-add.validator'
+import { ValidatorService } from '../../../shared/services/validator/validator.service'
 export class LabAddController implements IController {
   private readonly labAddService:LabAddService
-  private readonly validatorProvider:IValidatorProvider<ObjectSchema>
+  private readonly validatorService:ValidatorService
 
-  constructor (labAddService:LabAddService, validatorProvider:IValidatorProvider<ObjectSchema>) {
+  constructor (labAddService:LabAddService, validatorService:ValidatorService) {
     this.labAddService = labAddService
-    this.validatorProvider = validatorProvider
+    this.validatorService = validatorService
   }
 
   handle = async (request: Request, response: Response):Promise<Response> => {
     try {
       const { body } = request
 
-      const params = await this.validatorProvider.validate<ILabAddDto>(validator(), body)
+      const params = await this.validatorService.execute<ILabAddDto>({ schema: validator(), params: body })
 
       if (typeof params !== 'object' || Array.isArray(params)) return badRequest(params, response)
 
